4f520846c1006ba035ead8109e073aadc4929a70
upcwangying <upcwangying@126.com>
2020-07-21 11:17:51 +0800

    feat: dynamic route

---
+++ samples-gateway/src/main/java/com/upcwangying/cloud/samples/gateway/controller/RouteController.java
@@ -0,0 +1,121 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 cloud.upcwangying.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package com.upcwangying.cloud.samples.gateway.controller;
+
+import com.upcwangying.cloud.samples.gateway.model.GatewayFilterDefinition;
+import com.upcwangying.cloud.samples.gateway.model.GatewayPredicateDefinition;
+import com.upcwangying.cloud.samples.gateway.model.GatewayRouteDefinition;
+import com.upcwangying.cloud.samples.gateway.service.DynamicRouteServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.FilterDefinition;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author WANGY
+ */
+@RestController
+@RequestMapping("/route")
+public class RouteController {
+
+    @Autowired
+    private DynamicRouteServiceImpl dynamicRouteService;
+
+    /**
+     * 增加路由
+     * @param gwdefinition
+     * @return
+     */
+    @PostMapping("/add")
+    public String add(@RequestBody GatewayRouteDefinition gwdefinition) {
+        try {
+            RouteDefinition definition = assembleRouteDefinition(gwdefinition);
+            return this.dynamicRouteService.add(definition);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "succss";
+    }
+
+    @GetMapping("/delete/{id}")
+    public Mono<ResponseEntity<Object>> delete(@PathVariable String id) {
+        return this.dynamicRouteService.delete(id);
+    }
+
+    @PostMapping("/update")
+    public String update(@RequestBody GatewayRouteDefinition gwdefinition) {
+        RouteDefinition definition = assembleRouteDefinition(gwdefinition);
+        return this.dynamicRouteService.update(definition);
+    }
+
+    private RouteDefinition assembleRouteDefinition(GatewayRouteDefinition gwdefinition) {
+
+        RouteDefinition definition = new RouteDefinition();
+
+        // ID
+        definition.setId(gwdefinition.getId());
+
+        // Predicates
+        List<PredicateDefinition> pdList = new ArrayList<>();
+        for (GatewayPredicateDefinition gpDefinition: gwdefinition.getPredicates()) {
+            PredicateDefinition predicate = new PredicateDefinition();
+            predicate.setArgs(gpDefinition.getArgs());
+            predicate.setName(gpDefinition.getName());
+            pdList.add(predicate);
+        }
+        definition.setPredicates(pdList);
+
+        // Filters
+        List<FilterDefinition> fdList = new ArrayList<>();
+        for (GatewayFilterDefinition gfDefinition: gwdefinition.getFilters()) {
+            FilterDefinition filter = new FilterDefinition();
+            filter.setArgs(gfDefinition.getArgs());
+            filter.setName(gfDefinition.getName());
+            fdList.add(filter);
+        }
+        definition.setFilters(fdList);
+
+        // URI
+        URI uri = UriComponentsBuilder.fromUriString(gwdefinition.getUri()).build().toUri();
+        definition.setUri(uri);
+
+        return definition;
+    }
+
+}
